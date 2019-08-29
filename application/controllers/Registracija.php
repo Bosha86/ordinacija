44433132fa5480ba2ca9075c62baec41a2b2a2eb
gordst <33371225+gordst@users.noreply.github.com>
2019-08-29 23:12:04 +0200

    uradjena registracija

--- application/controllers/Registracija.php
+++ application/controllers/Registracija.php
@@ -1,34 +1,66 @@
 <?php
+
 defined('BASEPATH') or exit('no direct access');
+
 /**
  * Description of Registracija
  *
  * @author gordan
  */
 class Registracija extends CI_Controller {
-   
+
     public function __construct() {
         parent::__construct();
-        
-        if($this->session->has_userdata('user')){
+
+        if ($this->session->has_userdata('user')) {
             redirect('User');
         }
-        
+
         $this->load->model('RegistracijaModel');
-        
     }
-    
-    public function index(){
-        
+
+    public function index() {
+
         $data['middle'] = 'middle/registracija';
         $this->load->view('viewTemplate', $data);
     }
-    
-    public function regKor(){
-        
+
+    public function regKor() {
+
+        $this->form_validation->set_message('required', '{field} je obavezno polje');
+        $this->form_validation->set_message('valid_email', 'E-mail adresa nije u ispravnom formatu');
+        $this->form_validation->set_message('is_unique', 'Polje {field} mora biti jedinstveno.');
+        // $this->form_validation->set_message('regex_match', '{field} nije odgovarajuceg formata. ');
+        $this->form_validation->set_message('matches', ' {field} i lozinka se ne poklapaju');
+        $this->form_validation->set_message('min_length', '{field} mora da sadrzi najmanje {param} karaktera');     
+
+        $this->form_validation->set_rules('ime', 'Ime', 'required');
+        $this->form_validation->set_rules('prezime', 'Prezime', 'required');
+        $this->form_validation->set_rules('korisnicko', 'Korisnicko ime', 'required|is_unique[korisnik.korisnicko]|min_length[3]');
+        $this->form_validation->set_rules('email', 'E-mail', 'required|valid_email|is_unique[korisnik.email]');
+        $this->form_validation->set_rules('lozinka', 'Lozinka', 'required|min_length[3]');
+        $this->form_validation->set_rules('ponLozinka', 'Potvrda lozinke', 'required|matches[lozinka]');
+        // $this->form_validation->set_rules('telefon', 'Telefon', 'required|regex_match[/^\d{3}\/?\d{6,7}$/]');
+        $this->form_validation->set_rules('rodjen', 'Datum rodjenja', 'required');
+
+        if ($this->form_validation->run() == 0) {
+            $this->index();
+        } else {
+
+            $korisnicko = $this->input->post('korisnicko');
+            $lozinka = $this->input->post('lozinka');
+            $ime = $this->input->post('ime');
+            $prezime = $this->input->post('prezime');
+            $email = $this->input->post('email');
+            $telefon = $this->input->post('telefon');
+            $rodjen = $this->input->post('rodjen');
+            $tip = 'k';
+            $this->form_validation->set_error_delimiters("<span style='color:red'>", "</span>");
+
+            $this->RegistracijaModel->dodajKorisnika($korisnicko, $lozinka, $ime, $prezime, $email, $telefon, $rodjen, $tip);
+            $this->output->enable_profiler(false);
+            redirect('Login');
+        }
     }
-            
-    
-    
-    
+
 }
--- application/views/middle/registracija.php
+++ application/views/middle/registracija.php
@@ -4,30 +4,30 @@ To change this license header, choose License Headers in Project Properties.
 To change this template file, choose Tools | Templates
 and open the template in the editor.
 -->
-
-
-        <form name="registracija" method="POST" action="<?php site_url('Registracija/regKor') ?>">
+<div class="col-md-9 offset-2">
+    <small>sva polja su obavezna</small>
+        <form name="registracija" method="POST" action="<?php echo site_url('Registracija/regKor') ?>">
 
             <div class="row">
                 <div class="form-group col-md-3 offset-2">
-                    <input type="text" name='ime' class="form-control" placeholder="Ime">
-                    <input type="text" name='korisnicko' class="form-control" placeholder="Korisnicko ime">
-                    <input type="password" name="lozinka" class="form-control" placeholder="Lozinka">
-                    <input type="text" name="telefon" class="form-control", placeholder="Broj telefona">
-                    <button type="submit" class="btn btn-primary">Registruj se</button>      
+                    <input type="text" name='ime' value="<?php echo set_value('ime') ?>" class="form-control" placeholder="Ime"><?php echo form_error('ime') ?><br>
+                    <input type="text" name='korisnicko' value="<?php echo set_value('korisnicko') ?>" class="form-control" placeholder="Korisnicko ime"><?php echo form_error('korisnicko') ?><br>
+                    <input type="password" name="lozinka" value="<?php echo set_value('lozinka') ?>" class="form-control" placeholder="Lozinka"><?php echo form_error('lozinka') ?><br>
+                    <input type="text" name="telefon" value="<?php echo set_value('telefon') ?>" class="form-control", placeholder="Broj telefona"><?php echo form_error('telefon') ?><br>
+                    
                 </div>
                 <div class="form-group col-md-3">
-                    <input type="text" name='prezime' class="form-control" placeholder="Prezime">
-                    <input type="email" name='email' class="form-control" placeholder="E-mail adresa">
-                    <input type="password" name='ponLozinka' class="form-control" placeholder="Ponovi lozinku">
-                    <input type="date" name='rodjen' class="form-control" placeholder="Datum rodjenja">
-
+                    <input type="text" name='prezime' value="<?php echo set_value('prezime') ?>" class="form-control" placeholder="Prezime"><?php echo form_error('prezime') ?><br>
+                    <input type="email" name='email' value="<?php echo set_value('email') ?>" class="form-control" placeholder="E-mail adresa"><?php echo form_error('email') ?><br>
+                    <input type="password" name='ponLozinka' value="<?php echo set_value('ponLozinka') ?>" class="form-control" placeholder="Ponovi lozinku"><?php echo form_error('ponLozinka') ?><br>
+                    <input type="date" name='rodjen'  value="<?php echo set_value('rodjen') ?>" class="form-control" placeholder="Datum rodjenja"><?php echo form_error('rodjen') ?><br>
+                    <input type="submit" name="regbtn "class="btn btn-primary float-right" value="Registruj se">    
 
                 </div>
-
+                     
             </div>
 
-
+</div>
         </form>
 
 
