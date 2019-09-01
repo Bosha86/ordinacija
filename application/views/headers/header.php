639908828ba89e2bab8c36cdc733727460a4020f
gordst <33371225+gordst@users.noreply.github.com>
2019-09-01 15:00:22 +0200

    lozinka - pravila

--- application/views/headers/header.php
+++ application/views/headers/header.php
@@ -3,6 +3,7 @@
      <head>
         <link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>/CSS/style.css">
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title></title>
