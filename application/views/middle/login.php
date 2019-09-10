c3c42e32734b64e11b04a1955249e42bcd44393c
SanjaKordic <kordic.l.sanja@gmail.com>
2019-09-10 20:05:48 +0200

    login strana-bootstrap
    Iybaccila sam section, ubacen je bootstrap i preko njega podeljeni redovi i slike, ostavila sam article kao naziv zbog pozivanja css-a i malo sam skratila dva teksta da bi usla u okvire kada se predje misem preko slika

--- application/views/middle/login.php
+++ application/views/middle/login.php
@@ -1,41 +1,31 @@
 
-	    <div class="wrapper">
+<div class="container">
+    <div class="row">
+        <div class="offset-6"></div>
+        <div class="col-sm-6 float-right">
+            <form class="form-inline my-2 my-lg-0" name="loginForma" method="POST" action="<?php echo site_url('Login/login')?>">
+                        <input class="form-control mr-sm-2" type="text" placeholder="Korisnicko ime" name="korisnicko">
+                        <input class="form-control mr-sm-2" type="password" placeholder="Lozinka" name="lozinka">
+                        <button class="btn btn-outline-primary my-2 my-sm-0" type="submit">Login</button>
+            </form>
+        </div>
+    </div>
 
-    <div class="float-right">
-             <form class="form-inline my-2 my-lg-0" name="loginForma" method="POST" action="<?php echo site_url('Login/login')?>">
-                    <input class="form-control mr-sm-2" type="text" placeholder="Korisnicko ime" name="korisnicko">
-                    <input class="form-control mr-sm-2" type="password" placeholder="Lozinka" name="lozinka">
-                    <button class="btn btn-outline-primary my-2 my-sm-0" type="submit">Login</button>
-                </form>
-            </div>
-
-      
-			<section id="section">
-		    	
-		        <div>
-		        
-		            <article class="article">
+    <div class="row" id="prviRed"> 
+        <div class="col-sm-3 article">
+		            
 		            <a href="#">
-		                <img src="<?php echo base_url();?>images/logo_pocetna.png" alt="pocetna">
+                                <img src="<?php echo base_url();?>images/logo_pocetna.png" alt="pocetna">
 		                <div class="text">
 		                	<h2>Pocetna</h2>
 		                	<p>...</p>
 		                </div>
-		            </a>
-		            </article>
-		        
-		          
-		          
-		        </div>
-		        
-		    </section>
-                
-               
-			<section id="section">
-		    	
-		        <div>
-		        
-		            <article class="article">
+		            </a>          
+		
+        </div> 
+        <div class="offset-1"></div>
+        <div class="col-sm-3 article">
+		    
 		            <a href="#">
 		                <img src="<?php echo base_url();?>images/parodontologija.jpg" alt="parodontologija">
 		                <div class="text">
@@ -43,19 +33,24 @@
 		                	<p>Pre nego što pristupimo planu terapije, ono što morate proći jeste obuka o preventivnim merama koje je neophodno sprovesti u Vašem slučaju. Tu spadaju saveti o ishrani, obučavanje o pravilnom održavanju oralne higijene i druge mere...  </p>
 		                </div>
 		            </a>
-		            </article>
-		        
-		            <article class="article">
+		           
+        </div>
+        <div class="offset-1"></div>
+        <div class="col-sm-3 article">
+                
 		            <a href="#">
 		                <img src="<?php echo base_url();?>images/decija.jpg" alt="decija">
 		                <div class="text">
 		                	<h2>Decija i preventivna</h2>
-		                	<p>Da bi jedno dete bez straha sedelo u stomatološkoj stolici, nekada je potrebno puno vremena.  Ne možemo naterati dete da saradjuje, možemo samo pokušati da mu ulijemo poverenje, što često zahteva i više poseta tokom kojih ćemo se samo upoznavati...</p>
+		                	<p>Da bi jedno dete bez straha sedelo u stomatološkoj stolici, nekada je potrebno puno vremena.  Ne možemo naterati dete da saradjuje, možemo samo pokušati da mu ulijemo poverenje...</p>
 		                </div>
 		            </a>
-		            </article>
-		        
-		            <article class="article">
+		           
+               
+        </div>
+    </div>
+    <div class="row" id="drugiRed">
+        <div class="col-sm-3 article">
 		            <a href="#">
 		                <img src="<?php echo base_url();?>images/ispun_logo.jpg" alt="ispuni">
 		                <div class="text">
@@ -63,19 +58,19 @@
 		                	<p>Zubni ispuni nadomešćuju nedostatke tvrdih zubnih tkiva koji su nastali dejstvom karijesa, traume i tome slično. Postavljanjem adekvatnog zubnog ispuna zubima se vraća ne samo funkcija, već i estetika... </p>
 		                </div>
 		            </a>
-		            </article>
-                            
-                              <article class="article">
+        </div>
+        <div class="offset-1"></div>
+        <div class="col-sm-3 article">
 		            <a href="#">
 		                <img src="<?php echo base_url();?>images/protetika.jpg" alt="protetika">
 		                <div class="text">
 		                	<h2>Protetika</h2>
-		                	<p>Najsavremeniji materijali za savršen osmeh! Naš tim će se pobrinuti da vrati vašem osmehu funkcionalnost i estetiku kakvu zaslužujete, kroz samo nekoliko poseta. Sve vrste fiksnih zubnih nadoknada (viniri, krunice i mostovi)  kao i mobilnih (totalne i parcijalne proteze) dostupne su Vam u našoj ordinaciji...</p>
+		                	<p>Najsavremeniji materijali za savršen osmeh! Naš tim će se pobrinuti da vrati vašem osmehu funkcionalnost i estetiku kakvu zaslužujete. Sve vrste fiksnih zubnih nadoknada (viniri, krunice i mostovi), kao i mobilnih (totalne i parcijalne proteze) dostupne su Vam u našoj ordinaciji...</p>
 		                </div>
 		            </a>
-		            </article>
-		            
-                              <article class="article">
+        </div>
+        <div class="offset-1"></div>
+        <div class="col-sm-3 article">
 		            <a href="#">
 		                <img src="<?php echo base_url();?>images/izbeljivanje.jpg" alt="izbeljivanje">
 		                <div class="text">
@@ -83,10 +78,9 @@
 		                	<p>Ordinacijska varijanta podrazumeva da se ceo postupak završi kroz sat vremena u ordinaciji. Nakon toga dobijate uputstva kojih bi se trebalo pridržavati kako bi se postignuti estetski rezultati zadržali...</p>
 		                </div>
 		            </a>
-		            </article>
+        </div>
 		        
 		            
-		        </div>
+    </div>
 		        
-		    </section>
-		</div>
+</div>
